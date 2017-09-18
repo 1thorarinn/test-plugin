b174978ba200029c16f8c3e9e0dc1c32336e9ce5
Þórarinn Ingi Tómasson <thorarinnt@tmsoftware.is>
2017-09-18 13:13:34 +0000

    Plugin update check

--- test-plugin.php
+++ test-plugin.php
@@ -8,3 +8,11 @@ Author URI: http://offorsi.is
 */
 
 
+require 'plugin_update_check.php';
+$MyUpdateChecker = new PluginUpdateChecker_2_0 (
+    'https://kernl.us/api/v1/updates/59bfc5c769f5da2ec903cb91/',
+    __FILE__,
+    'test-plugin',
+    1
+);
+// $MyUpdateChecker->purchaseCode = "somePurchaseCode";  <---- Optional!
